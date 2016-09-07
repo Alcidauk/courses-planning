035a6fd7416dd29591b91341337a467b6b2ceb68
alcidauk <pir.r25@gmail.com>
2016-09-07 21:15:33 +0200

    - add automatic creation of planning period if it doesn't exists

--- src/main/java/com/alcidauk/ui/calendar/worksession/WorkSessionCalendarEventProvider.java
+++ src/main/java/com/alcidauk/ui/calendar/worksession/WorkSessionCalendarEventProvider.java
@@ -1,13 +1,7 @@
 package com.alcidauk.ui.calendar.worksession;
 
-import com.alcidauk.data.bean.DefaultUnavailabilitySession;
-import com.alcidauk.data.bean.PlanningPeriod;
-import com.alcidauk.data.bean.WorkSession;
-import com.alcidauk.data.bean.WorkSessionType;
-import com.alcidauk.data.repository.DefaultUnavailabilitySessionRepository;
-import com.alcidauk.data.repository.PlanningPeriodRepository;
-import com.alcidauk.data.repository.WorkSessionRepository;
-import com.alcidauk.data.repository.WorkSessionTypeRepository;
+import com.alcidauk.data.bean.*;
+import com.alcidauk.data.repository.*;
 import com.alcidauk.ui.calendar.CalendarUtils;
 import com.alcidauk.ui.dto.WorkSessionCalendarEventBean;
 import com.vaadin.ui.Calendar;
@@ -49,8 +43,12 @@ public class WorkSessionCalendarEventProvider implements CalendarEventProvider {
                 Instant.ofEpochSecond(start.toInstant().getEpochSecond()), Instant.ofEpochSecond(end.toInstant().getEpochSecond()));
 
         if(planningPeriod == null){
-            log.error(String.format("Error retrieving planning for dates %s to %s.", start.toString(), end.toString()));
-        } else if(!planningPeriod.isDefaultSessionsGenerated()){
+            log.info(String.format("Creating planning period for dates %s to %s.", start.toString(), end.toString()));
+            planningPeriod = createPlanningPeriod(Instant.ofEpochSecond(start.toInstant().getEpochSecond()),
+                    Instant.ofEpochSecond(end.toInstant().getEpochSecond()));
+        }
+
+        if(!planningPeriod.isDefaultSessionsGenerated()){
             generateDefaultSessionsAsEvents(start, workSessionTypeRepository.findByName("unavailable"));
             planningPeriod.setDefaultSessionsGenerated(true);
             planningPeriodRepository.save(planningPeriod);
@@ -59,6 +57,16 @@ public class WorkSessionCalendarEventProvider implements CalendarEventProvider {
         List<WorkSession> workSessions = workSessionRepository.findBetweenStartInstantAndEndInstant(start.toInstant(), end.toInstant());
         log.info(workSessions.size() + "found !");
         return workSessions.stream().map(WorkSessionCalendarEventBean::new).collect(Collectors.toList());
+    }
+
+    private PlanningPeriod createPlanningPeriod(Instant startInstant, Instant endInstant) {
+        PlanningPeriod planningPeriod = new PlanningPeriod(startInstant, endInstant, getPlanningPeriodTypes(), false);
+        planningPeriodRepository.save(planningPeriod);
+        return planningPeriod;
+    }
+
+    private List<PlanningPeriodEventType> getPlanningPeriodTypes() {
+        return null;
     }
 
     private void generateDefaultSessionsAsEvents(Date start, WorkSessionType unavailableWorkSessionType) {
